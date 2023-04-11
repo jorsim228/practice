0af59621f0c89508b830d2bcef137b84829df881
jorsim228 <130034432+jorsim228@users.noreply.github.com>
2023-04-11 13:52:54 +0300

    Init

---
+++ BackendApi/Models/Filter.cs
@@ -0,0 +1,16 @@
+﻿using BackendApi.Models;
+using System;
+using System.Collections.Generic;
+
+namespace Online_shop_database.Models;
+
+public partial class Filter
+{
+    public int FilterId { get; set; }
+
+    public string FilterName { get; set; } = null!;
+
+    public int CategoryId { get; set; }
+
+    public virtual Category Category { get; set; } = null!;
+}
